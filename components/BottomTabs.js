6c2807b9178d0fd897c8db8b1108eea408360d35
dagurvishal <dagurvishal2049@gmail.com>
2026-01-23 01:03:33 +0530

    Update BottomTabs.js

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,1 +1,32 @@
+"use client";
 
+export default function BottomTabs({ tab, setTab }) {
+  return (
+    <div className="tabsBar">
+      <div className="tabsInner">
+        <div className="tabsRow">
+          <button
+            className={`tabBtn ${tab === "home" ? "tabBtnActive" : ""}`}
+            onClick={() => setTab("home")}
+          >
+            🎬 Browse
+          </button>
+
+          <button
+            className={`tabBtn ${tab === "trending" ? "tabBtnActive" : ""}`}
+            onClick={() => setTab("trending")}
+          >
+            ⚡ Latest
+          </button>
+
+          <button
+            className={`tabBtn ${tab === "request" ? "tabBtnActive" : ""}`}
+            onClick={() => setTab("request")}
+          >
+            📨 Request
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
