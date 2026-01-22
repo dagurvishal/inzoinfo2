df36e48bec8212d846eecb7e715137daffd2c1c9
dagurvishal <dagurvishal2049@gmail.com>
2026-01-23 01:03:50 +0530

    Update MovieCard.js

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -1,1 +1,15 @@
+"use client";
 
+export default function MovieCard({ movie, onOpen }) {
+  return (
+    <div
+      className="card"
+      style={{ padding: 10, cursor: "pointer" }}
+      onClick={onOpen}
+    >
+      <img className="poster" src={movie.poster_url} alt={movie.name} />
+      <div className="movieTitle">{movie.name}</div>
+      <div className="movieMeta">{movie.category}</div>
+    </div>
+  );
+}
